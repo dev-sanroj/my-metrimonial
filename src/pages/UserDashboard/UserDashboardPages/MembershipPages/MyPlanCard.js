915a15b8ccd85f641f9c2692e444f12e7e1a029f
CodeExplorer-Mohit <jatavmohit747.com>
2024-06-16 15:33:50 +0530

    Updated files

--- src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlanCard.js
+++ src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlanCard.js
@@ -4,16 +4,16 @@ import Button from "../../../../components/UI/Button/Button";
 const MyplanCard = () => {
   return (
     <ul className="flex justify-between items-center w-full bg-[#FFFFFF]">
-      <li className="flex justify-center items-center bg-blue-200 text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[110px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[110px]">
         Platinum
       </li>
-      <li className="flex justify-center items-center bg-blue-200 text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[35%] min-h-[110px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[35%] min-h-[110px]">
         12 Months(May 2024-June 2024)
       </li>
-      <li className="flex justify-center items-center bg-blue-200 text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[15%] min-h-[110px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[15%] min-h-[110px]">
         Cost
       </li>
-      <li className="flex justify-center items-center bg-blue-200 text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[110px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[110px]">
         <FaDownload className="text-[18px] md:hidden" />
         <Button
           text="DOWNLOAD"

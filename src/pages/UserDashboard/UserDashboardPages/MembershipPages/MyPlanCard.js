86ffae8d0b0b759dd9fac220b83c811719aedf18
CodeExplorer-Mohit <jatavmohit747.com>
2024-06-16 15:27:37 +0530

    Updated files

--- src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlanCard.js
+++ src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlanCard.js
@@ -1,21 +1,23 @@
+import { FaDownload } from "react-icons/fa6";
 import Button from "../../../../components/UI/Button/Button";
 
 const MyplanCard = () => {
   return (
     <ul className="flex justify-between items-center w-full bg-[#FFFFFF]">
-      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] h-[60px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[100px]">
         Platinum
       </li>
-      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[35%] h-[60px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[35%] min-h-[100px]">
         12 Months(May 2024-June 2024)
       </li>
-      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[15%] h-[60px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[15%] min-h-[100px]">
         Cost
       </li>
-      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] h-[60px]">
+      <li className="flex justify-center items-center text-[14px] lg:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] min-h-[100px]">
+        <FaDownload className="text-[18px] md:hidden" />
         <Button
           text="DOWNLOAD"
-          className="rounded-none h-[40px] w-[180px]"
+          className="hidden md:flex rounded-none h-[40px] w-[140px] md:w-[160px] lg:w-[180px]"
           bgBtn="bg-[#000000] font-semibold"
         />
       </li>
--- src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlans.js
+++ src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlans.js
@@ -3,21 +3,23 @@ import MyPlanCard from "./MyPlanCard";
 const MyPlans = (props) => {
   return (
     <div className="flex justify-center items-start p-2 md:p-4 bg-[#FFFFF4] shadow-[0_0_8px_rgba(0,0,0,0.1)] w-full rounded-lg mt-4">
-      <div className="flex flex-col justify-start items-start w-full overflow-y-scroll min-h-[480px] max-h-[500px] pr-4">
+      <div className="flex flex-col justify-start items-start w-full overflow-y-scroll min-h-[480px] max-h-[500px] pr-2 md:pr-4">
         <ul className="flex justify-between items-center w-full bg-[#FFFFFF]">
-          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] h-[60px]">
+          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] md:p-2 text-center border-[1px] w-[25%] h-[60px]">
             Plan type
           </li>
-          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[35%] h-[60px]">
+          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] md:p-2 text-center border-[1px] w-[35%] h-[60px]">
             Duration
           </li>
-          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[15%] h-[60px]">
+          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] md:p-2 text-center border-[1px] w-[15%] h-[60px]">
             Cost
           </li>
-          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] p-2 text-center border-[1px] w-[25%] h-[60px]">
+          <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] md:p-2 text-center border-[1px] w-[25%] h-[60px]">
             Invoice
           </li>
         </ul>
+        <MyPlanCard />
+        <MyPlanCard />
         <MyPlanCard />
       </div>
     </div>

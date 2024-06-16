cbe1d4ec7b0b73692b397dc579b016a6db23beb9
CodeExplorer-Mohit <jatavmohit747.com>
2024-06-16 15:38:55 +0530

    Updated files

--- src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlans.js
+++ src/pages/UserDashboard/UserDashboardPages/MembershipPages/MyPlans.js
@@ -3,7 +3,7 @@ import MyPlanCard from "./MyPlanCard";
 const MyPlans = (props) => {
   return (
     <div className="flex justify-center items-start p-2  md:p-4 bg-[#FFFFF4] shadow-[0_0_8px_rgba(0,0,0,0.1)] w-full rounded-lg mt-4">
-      <div className="flex flex-col justify-start items-start w-full overflow-y-scroll min-h-[480px] max-h-[500px] pr-2 md:pr-4">
+      <div className="flex flex-col justify-start items-start w-full overflow-y-scroll min-h-[480px] max-h-[500px] ">
         <ul className="flex justify-between items-center w-full bg-[#FFFFFF]">
           <li className="flex justify-center items-center font-bold text-[14px] md:text-[16px] text-[#212529] md:p-2 text-center border-[1px] w-[25%] h-[60px]">
             Plan type

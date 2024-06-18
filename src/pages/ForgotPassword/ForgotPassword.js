276d11bbcd9e68928e467cbb9e99af86d7d35fbf
CodeExplorer-Mohit <jatavmohit747.com>
2024-06-18 10:50:55 +0530

    Updated files

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -5,16 +5,18 @@ import ForgotPasswordForm from "./ForgotPasswordForm";
 const ForgotPassword = (props) => {
   return (
     <Modal onHideSignup={props.onHideForgotPassword}>
-      <div className="flex justify-center items-center relative py-5 border p-4">
-        <IoCloseSharp
-          onClick={props.onHideForgotPassword}
-          className="absolute active:text-[#333333] text-[#333333] hover:text-[#FF4061] cursor-pointer right-2 top-2"
-          size={28}
-        />
+      <div className="flex flex-col justify-center items-center relative py-3 md:p-4 w-full lg:min-w-full">
+        <div className="flex justify-center items-center w-full mb-2">
+          <IoCloseSharp
+            onClick={props.onHideForgotPassword}
+            className="absolute active:text-[#333333] text-[#333333] hover:text-[#FF4061] cursor-pointer -right-1 -top-1 md:right-2 md:top-2"
+            size={28}
+          />
+        </div>
 
-        <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-col justify-center items-start min-w-[400px]">
-            <h2 className="font-extrabold text-[32px] text-[#333333]">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="flex flex-col justify-center items-center w-full">
+            <h2 className="font-extrabold text-[26px] md:text-[32px] text-[#333333] text-center">
               Reset Your Password
             </h2>
           </div>

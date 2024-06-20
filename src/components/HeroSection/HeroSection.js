f1bdda1d2cbefe4df4a7b4ff02378b1f91df3459
CodeExplorer-Mohit <jatavmohit747.com>
2024-06-20 13:59:33 +0530

    Updated files

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,12 @@
 import FindYourMatchForm from "./FindYourMatchForm";
 
-const HeroSection = () => {
+const HeroSection = (x) => {
   return (
+    // <section
+    //   className={`relative flex justify-center items-center w-[100%] mt-4 h-[100vh] lg:h-[100vh] bg-[url('https://img.freepik.com/free-vector/abstract-luxury-beautiful-decorative-yellow-background_1055-6454.jpg?t=st=1718832305~exp=1718835905~hmac=7020a2b099f1cc80e5235812348d804df321c740ef605ff4a6fc7931248826c2&w=740')] bg-cover bg-no-repeat  bg-top`}
+    // >
     <section
-      className={`relative flex justify-center items-center w-[100%] mt-4 h-[100vh] lg:h-[100vh] bg-[url('https://t3.ftcdn.net/jpg/06/68/19/00/240_F_668190040_tBb4FVvIeGd9RnZhcCh8z3nG9DKfmHOj.jpg')] bg-cover bg-no-repeat  bg-top`}
+      className={`relative flex justify-center items-center w-[100%] mt-4 h-[100vh] lg:h-[100vh]`}
     >
       <span className="absolute z-0 left-0 top-0 bg-[rgba(0,0,0,0.2)] w-[100%] h-[100%]"></span>
       <div className="relative z-10 flex flex-col justify-center items-center w-[90%] lg:w-full">
